f360acc22da6e8e01dfd232c2213f25f49e74245
Iaroslav Karandashev <yarix7@gmail.com>
2017-09-30 19:06:34 -0700

    qwer

--- client.py
+++ client.py
@@ -2,10 +2,14 @@ import bluetooth
 import json
 import time
 import requests
+from satori.rtm.client import make_client
 
 client_id="a2fd64e4-0d67-4545-b353-475f14123ee0"
 client_secret="H7xS3yK2oS3uO0hU4xL5dQ4aI6rL7kC8cJ5uG2lM6cW8fM7tV2"
 building_id=9990000508
+
+endpoint = "wss://n0cy74ab.api.satori.com"
+appkey = "CF1EDbdeFC3aC712Ac8cAB5bbdDF8fdB
 
 headers = {
     'x-ibm-client-id': client_id,
@@ -13,6 +17,24 @@ headers = {
     'accept': "application/vnd.collection+json",
     'content-type': "application/vnd.collection+json"
     }
+    
+def publish_user(building_id, mac):
+    with make_client(endpoint=endpoint, appkey=appkey) as client:
+        print('Connected to Satori RTM!')
+
+        while True:
+
+            def on_publish_ack(pdu):
+                if pdu['action'] == 'rtm/publish/ok':
+                    print('Publish confirmed')
+                else:
+                    print(
+                        'Failed to publish. '
+                        'RTM replied with the error {0}: {1}'.format(
+                            pdu['body']['error'], pdu['body']['reason']))
+
+            message = {"building": building_id, 'user': mac}
+            client.publish(u"checkins", message, callback=on_publish_ack)
 
 def post_elevator_call(building_id, from_area, to_area):
     payload = """{
@@ -42,3 +64,4 @@ while True:
 		if mac in booked_clients:
 			print 'call lift for %s' % name
 			post_elevator_call(building_id,0,1)
+		  publish_user(building_id, name)
